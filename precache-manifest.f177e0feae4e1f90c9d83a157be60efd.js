39996d73e07d3e8d3f25ead1513d0e08214f8c6e
Alexander Jurcau <jurcaua@gmail.com>
2020-08-22 01:02:41 -0400

    Updates

---
+++ precache-manifest.f177e0feae4e1f90c9d83a157be60efd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99844b9514688cf0ca64e32de5362693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06e83e57c0127606a60",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "dbc7f449f5a6f091ec79",
+    "url": "/static/css/main.81bec9f3.chunk.css"
+  },
+  {
+    "revision": "b06e83e57c0127606a60",
+    "url": "/static/js/2.ed605b06.chunk.js"
+  },
+  {
+    "revision": "ed5542cb29edbbcaf3e489650cfeb048",
+    "url": "/static/js/2.ed605b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc7f449f5a6f091ec79",
+    "url": "/static/js/main.a4dbf83b.chunk.js"
+  },
+  {
+    "revision": "893c75ef89f57ce76a52",
+    "url": "/static/js/runtime-main.a4d5e0ee.js"
+  },
+  {
+    "revision": "f269c193b1ef0b739030d56386670c3f",
+    "url": "/static/media/NotoSansCJKjp-Light.f269c193.otf"
+  }
+]);
