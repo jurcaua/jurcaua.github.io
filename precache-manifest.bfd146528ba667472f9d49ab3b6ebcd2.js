43f30fa842b7f6c0fc6b21106f9d6fdfdbbdd96f
Alexander Jurcau <jurcaua@gmail.com>
2024-08-25 13:05:06 +0900

    Updates

--- precache-manifest.bfd146528ba667472f9d49ab3b6ebcd2.js
+++ precache-manifest.bfd146528ba667472f9d49ab3b6ebcd2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "816c3ee813c6e79f0458ad9522070264",
+    "revision": "7bf3a860aed0da298e58ecba7691ca63",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f050fc0f.chunk.css"
   },
   {
-    "revision": "e7b4a32491a777e39a0c",
+    "revision": "d07a903dca9ed20430dc",
     "url": "/static/css/main.11c6d420.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2337711c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e7b4a32491a777e39a0c",
-    "url": "/static/js/main.dea5328f.chunk.js"
+    "revision": "d07a903dca9ed20430dc",
+    "url": "/static/js/main.b8c3d67a.chunk.js"
   },
   {
     "revision": "893c75ef89f57ce76a52",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.647fc159a83f49525e237b312042b788.js"
+  "/precache-manifest.bfd146528ba667472f9d49ab3b6ebcd2.js"
 );
 
 self.addEventListener('message', (event) => {
